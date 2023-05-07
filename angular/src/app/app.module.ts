771afc8df9065dd041b434bbc608e97efcf9efe2
mp299 <114026858+mp299@users.noreply.github.com>
2023-05-06 21:04:01 -0400

    vercel deployment is a pain

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+    BrowserModule,
+    AppRoutingModule
+  ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
