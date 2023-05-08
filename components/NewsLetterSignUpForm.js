fd0de0b297a3c333d0c322bcd566441e0894ec39
hehjunlim <hl62@njit.edu>
2023-05-08 16:13:48 -0400

    Allows page to be shared on social media

---
+++ components/NewsLetterSignUpForm.js
@@ -0,0 +1,46 @@
+import { useRef } from 'react';
+
+export default function NewsLetterSignUpForm() {
+  const inputRef = useRef(null);
+
+  const subscribeUser = async (e) => {
+    e.preventDefault();
+
+    // this is where your mailchimp request is made
+
+    const res = await fetch('/api/subscribeUser', {
+      body: JSON.stringify({
+        email: inputRef.current.value,
+      }),
+
+      headers: {
+        'Content-Type': 'application/json',
+      },
+
+      method: 'POST',
+    });
+  };
+
+  return (
+    <form onSubmit={subscribeUser}>
+      <label htmlFor="email-input" className="form__label">
+        Your Best Email
+      </label>
+
+      <input
+        type="email"
+        id="email-input"
+        name="email"
+        placeholder="your best email"
+        ref={inputRef}
+        required
+        autoCapitalize="off"
+        autoCorrect="off"
+      />
+
+      <button type="submit" value="" name="subscribe">
+        Subscribe
+      </button>
+    </form>
+  );
+}
