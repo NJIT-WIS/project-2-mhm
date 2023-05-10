da906f4e95babb5ebd5fdf431b84b93596ec121d
MikeMassotto <mm2529@njit.edu>
2023-05-09 22:20:10 -0400

    cringe logo but whatever

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -1,12 +1,10 @@
 import Link from 'next/link';
 import styles from '../styles/NavMenu.module.css';
-/*import styles from '../styles/global.module.css';*/
-import logo from '../images/earth.png';
 
 export default function NavMenu() {
   return (
     <nav className={styles.navMenu}>
-      <img src={require('../images/earth.png')} alt="Logo" />
+      <img src="https://cdn4.iconfinder.com/data/icons/education-196/52/Education__EarthGlobe__GraduationCap__GraduationCaps__Planet__Earth__Globe__Tool__Tools-512.png" alt="Logo" className={styles.logo} />
       <ul>
         <li><Link href="/">Home</Link></li>
         <li><Link href="/blog">Blog</Link></li>
@@ -16,4 +14,3 @@ export default function NavMenu() {
     </nav>
   )
 }
-
