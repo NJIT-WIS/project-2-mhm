0333eaa52dfcc31e5ad228f69209e577984f4843
hehjunlim <hl62@njit.edu>
2023-05-10 19:30:50 -0400

    enabled sharing on social media in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import styles from '../styles/foot.module.css';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 export default function Footer() {
   return (
@@ -7,6 +9,27 @@ export default function Footer() {
       <div className={styles.footerContainer}>
         <div className={styles.footerLogo}>
           <img src="https://cdn4.iconfinder.com/data/icons/education-196/52/Education__EarthGlobe__GraduationCap__GraduationCaps__Planet__Earth__Globe__Tool__Tools-512.png" alt="Logo" className={styles.logo} />
+        </div>
+        <div
+          style={{
+            marginTop: 10,
+            marginBottom: 15,
+            height: 40,
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}
+        >
+          <ButtonGroup aria-label="Basic example">
+            <a href="https://facebook.com" target='_blank'>
+              <Button variant="secondary">Facebook</Button>
+            </a>
+            <a href="https://linkedin.com" target='_blank'>
+              <Button variant="secondary">LinkedIn</Button>
+            </a>
+            <a href="https://twitter.com" target='_blank'>
+            <Button variant="secondary">Twitter</Button>
+            </a>
+          </ButtonGroup>
         </div>
         <nav className={styles.footerNav}>
           <ul>
@@ -17,7 +40,7 @@ export default function Footer() {
               <Link href="/sign up">Sign Up</Link>
             </li>
             <li>
-                <Link href="/privacy">Privacy Policy</Link>
+              <Link href="/privacy">Privacy Policy</Link>
             </li>
           </ul>
         </nav>
