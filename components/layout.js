4b20e2c5acb2c828ceb956c1947cb374405cfd50
hehjunlim <hl62@njit.edu>
2023-05-10 11:15:23 -0400

    fixed url defined error

--- components/layout.js
+++ components/layout.js
@@ -2,30 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Script from 'next/script'
 
-import styles from './layout.module.css'
+import styles from '../styles/global.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
 export const siteTitle = 'MyWebClass.org'
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Script
         src="https://connect.facebook.net/en_US/sdk.js"
@@ -43,7 +29,6 @@ export default function Layout({ children, home }) {
               className={utilStyles.borderCircle}
               height={144}
               width={144}
-              alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
@@ -56,12 +41,10 @@ export default function Layout({ children, home }) {
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
-                alt={name}
               />
             </Link>
             <h2 className={utilStyles.headingLg}>
               <Link href="/" className={utilStyles.colorInherit}>
-                {name}
               </Link>
             </h2>
           </>
