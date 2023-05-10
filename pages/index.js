ca2554b89d864f4dadc146f79ac7755a86f77abf
MikeMassotto <mm2529@njit.edu>
2023-05-09 20:19:09 -0400

    just realized I'm on main. Pushing a stable version of my work before I continue.

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
       <main>
         <section>
           <h2>Learn Tech Skills with MyWebClass.org</h2>
-          <p>Are you a student or a teacher looking to improve your tech skills? Look no further than MyWebClass.org. We offer a wide range of courses in programming, web development, graphic design, and more. Our courses are designed to help you succeed in your career or your studies.</p>
-          <p>With a monthly subscription, you can access all of our courses at any time. We offer flexible learning options to fit your schedule and your budget.</p>
+          <p>Looking to improve your tech skills? Look no further!</p>
+          <p>Our courses are designed to help you succeed in your career or your studies.</p>
         </section>
       </main>
       <Footer />
