226c41c99306f7d8ce0b5e1606502f5633124dfa
mp299 <114026858+mp299@users.noreply.github.com>
2023-05-10 23:22:52 -0400

    added test to check font

--- tests/accessibility_test.spec.js
+++ tests/accessibility_test.spec.js
@@ -7,21 +7,21 @@ const { pages } = require(path.join(process.cwd(), 'tests', 'pages.json'));
 
 const TIMEOUT = 30000;
 
-async function checkPageTitle(pageUrl, expectedTitle) {
+async function checkAccessibility(pageUrl) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto(pageUrl, { timeout: TIMEOUT });
-  const pageTitle = await page.title();
+  const fontSize = await page.$eval('body', (el) => parseFloat(getComputedStyle(el).fontSize));
+  expect(fontSize).toBeGreaterThan(10);
+
   await browser.close();
-  expect(pageTitle).toBe(expectedTitle);
 }
 
 pages.forEach((page) => {
-  test(`Page "${page.path}" should have the correct title`, async ({}) => {
+  test(`Page "${page.path}" should have the correct font size`, async ({}) => {
     console.log(page.path)
     const pageUrl = `${config.use.baseURL}${page.path}`;
 
-    const expectedTitle = page.title;
-    await checkPageTitle(pageUrl, expectedTitle);
+    await checkAccessibility(pageUrl);
   });
 });
