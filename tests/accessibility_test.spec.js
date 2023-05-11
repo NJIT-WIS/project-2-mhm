2cbee6022ab16d30487e29c035fec16015b6324f
mp299 <114026858+mp299@users.noreply.github.com>
2023-05-11 00:38:14 -0400

    chatGPT changes

--- tests/accessibility_test.spec.js
+++ tests/accessibility_test.spec.js
@@ -11,24 +11,27 @@ async function checkAccessibility(pageUrl) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto(pageUrl, { timeout: TIMEOUT });
+
+  // Check font size
   const fontSize = await page.$eval('body', (el) => parseFloat(getComputedStyle(el).fontSize));
   expect(fontSize).toBeGreaterThan(14);
 
- // Check Facebook link
-  //const facebookLink = await page.$('[aria-label="facebook"]');
-  //expect(facebookLink).toBeTruthy();
-  //await facebookLink.click();
-  //const newUrl = page.url();
-  //expect(newUrl).toContain('facebook.com');
+  // Check Facebook link
+  const facebookLink = await page.$('[aria-label="facebook"]');
+  expect(facebookLink).toBeTruthy();
+  await facebookLink.click();
+  const newUrl = page.url();
+  expect(newUrl).toContain('facebook.com');
 
   await browser.close();
 }
 
 pages.forEach((page) => {
-  test(`Page "${page.path}" should have the correct font size`, async ({}) => {
+  test(`Page "${page.path}" should have the correct font size and Facebook link should work`, async ({}) => {
     console.log(page.path)
     const pageUrl = `${config.use.baseURL}${page.path}`;
 
     await checkAccessibility(pageUrl);
   });
 });
+
